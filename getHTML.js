2720b559f32abb96733e7795a7fd5363d4ccef49
marcgregi <marc@hypedigital.co>
2019-01-14 18:56:58 +0000

    export module

---
+++ getHTML.js
@@ -0,0 +1,31 @@
+var https = require('https');
+
+var getHTML = function (options, callback) {
+
+
+  // callback function that accesses the server information
+  https.get(options, function(response) {
+  console.log("\n Data stream recieved: ");
+
+  // callback function
+  response.on('data', function(d) {
+    process.stdout.write(d);
+    console.log("\n data is split into chunks");
+  });
+
+});
+
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML);
+
+module.exports = getHTML;
